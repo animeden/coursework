8fc3cd2ce79a201ed71a781da2db1efcd1e27ad5
Denis Holei <goleiden@gmail.com>
2020-05-11 20:01:33 +0300

    first commit

--- src/main/java/edu/holeiden/coursework/controller/web/BrigadeWEBController.java
+++ src/main/java/edu/holeiden/coursework/controller/web/BrigadeWEBController.java
@@ -1,13 +1,14 @@
 package edu.holeiden.coursework.controller.web;
 
+import edu.holeiden.coursework.form.AdministrationForm;
+import edu.holeiden.coursework.form.BrigadeForm;
+import edu.holeiden.coursework.model.Administration;
 import edu.holeiden.coursework.model.Brigade;
 import edu.holeiden.coursework.service.brigade.impls.BrigadeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +30,49 @@ public class BrigadeWEBController {
         service.delete(id);
         model.addAttribute("brigades", service.getall());
         return "brigadeList";
+    }
+
+    @RequestMapping("/create")
+    String create(Model model){
+        BrigadeForm brigadeForm = new BrigadeForm();
+        model.addAttribute("brigadeForm", brigadeForm);
+        return "brigadeAdd";
+    }
+
+    @PostMapping("/create")
+    String create(Model model, @ModelAttribute("brigadeForm") BrigadeForm brigadeForm){
+        Brigade brigade = new Brigade();
+        brigade.setMission(brigadeForm.getMission());
+        brigade.setNumberOfWorkers(brigadeForm.getNumberOfWorkers());
+        brigade.setDepartmentId(brigadeForm.getDepartmentId());
+        brigade.setDescriction(brigadeForm.getDescriction());
+        service.save(brigade);
+        model.addAttribute("brigades", service.getall());
+        return "redirect:/web/brigade/get/list";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    String edit(Model model, @PathVariable("id") String id){
+        Brigade brigade = service.get(id);
+        BrigadeForm brigadeForm = new BrigadeForm();
+        brigadeForm.setMission(brigade.getMission());
+        brigadeForm.setNumberOfWorkers(brigade.getNumberOfWorkers());
+        brigadeForm.setDepartmentId(brigade.getDepartmentId());
+        brigadeForm.setDescriction(brigade.getDescriction());
+        model.addAttribute("brigadeForm", brigadeForm);
+        return "brigadeAdd";
+    }
+
+    @PostMapping("/edit/{id}")
+    String edith(Model model, @PathVariable("id") String id, @ModelAttribute("brigadeForm") BrigadeForm brigadeForm){
+        Brigade brigade = new Brigade();
+        brigade.setId(id);
+        brigade.setMission(brigadeForm.getMission());
+        brigade.setNumberOfWorkers(brigadeForm.getNumberOfWorkers());
+        brigade.setDepartmentId(brigadeForm.getDepartmentId());
+        brigade.setDescriction(brigadeForm.getDescriction());
+        service.save(brigade);
+        model.addAttribute("brigades", service.getall());
+        return "redirect:/web/brigade/get/list";
     }
 }
