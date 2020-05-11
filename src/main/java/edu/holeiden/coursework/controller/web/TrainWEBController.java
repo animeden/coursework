047c37d3b9fe77dd8bb2b08b6c5c99d08ca9de0d
Denis Holei <goleiden@gmail.com>
2020-05-11 22:03:43 +0300

    first commit

--- src/main/java/edu/holeiden/coursework/controller/web/TrainWEBController.java
+++ src/main/java/edu/holeiden/coursework/controller/web/TrainWEBController.java
@@ -1,13 +1,14 @@
 package edu.holeiden.coursework.controller.web;
 
+import edu.holeiden.coursework.form.AdministrationForm;
+import edu.holeiden.coursework.form.TrainForm;
+import edu.holeiden.coursework.model.Administration;
 import edu.holeiden.coursework.model.Train;
 import edu.holeiden.coursework.service.train.impls.TrainServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +29,50 @@ public class TrainWEBController {
                   @PathVariable("id") String id){
         service.delete(id);
         model.addAttribute("trains", service.getall());
-        return "trainList";
+        return "redirect:/web/train/get/list";
+    }
+
+    @RequestMapping("/create")
+    String create(Model model){
+        TrainForm trainForm = new TrainForm();
+        model.addAttribute("trainForm", trainForm);
+        return "trainAdd";
+    }
+
+    @PostMapping("/create")
+    String create(Model model, @ModelAttribute("trainForm") TrainForm trainForm){
+        Train train = new Train();
+        train.setModel(trainForm.getModel());
+        train.setTrainBrigade(trainForm.getTrainBrigade());
+        train.setRecoveryBrigade(trainForm.getRecoveryBrigade());
+        train.setDescriction(trainForm.getDescriction());
+        service.save(train);
+        model.addAttribute("trains", service.getall());
+        return "redirect:/web/train/get/list";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    String edit(Model model, @PathVariable("id") String id){
+        Train train = service.get(id);
+        TrainForm trainForm = new TrainForm();
+        trainForm.setModel(train.getModel());
+        trainForm.setTrainBrigade(train.getTrainBrigade());
+        trainForm.setRecoveryBrigade(train.getRecoveryBrigade());
+        trainForm.setDescriction(train.getDescriction());
+        model.addAttribute("trainForm", trainForm);
+        return "trainAdd";
+    }
+
+    @PostMapping("/edit/{id}")
+    String edith(Model model, @PathVariable("id") String id, @ModelAttribute("trainForm") TrainForm trainForm){
+        Train train = new Train();
+        train.setId(id);
+        train.setModel(trainForm.getModel());
+        train.setTrainBrigade(trainForm.getTrainBrigade());
+        train.setRecoveryBrigade(trainForm.getRecoveryBrigade());
+        train.setDescriction(trainForm.getDescriction());
+        service.save(train);
+        model.addAttribute("trains", service.getall());
+        return "redirect:/web/train/get/list";
     }
 }
