390d01d182f9d340e7c67e521619b5717a716254
Denis Holei <goleiden@gmail.com>
2020-05-11 20:58:44 +0300

    first commit

--- src/main/java/edu/holeiden/coursework/controller/web/DepartmentWEBController.java
+++ src/main/java/edu/holeiden/coursework/controller/web/DepartmentWEBController.java
@@ -1,13 +1,14 @@
 package edu.holeiden.coursework.controller.web;
 
+import edu.holeiden.coursework.form.BrigadeForm;
+import edu.holeiden.coursework.form.DepartmentForm;
+import edu.holeiden.coursework.model.Brigade;
 import edu.holeiden.coursework.model.Department;
 import edu.holeiden.coursework.service.department.impls.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +29,53 @@ public class DepartmentWEBController {
                   @PathVariable("id") String id){
         service.delete(id);
         model.addAttribute("departments", service.getall());
-        return "departmentList";
+        return "redirect:/web/department/get/list";
+    }
+
+    @RequestMapping("/create")
+    String create(Model model){
+        DepartmentForm departmentForm = new DepartmentForm();
+        model.addAttribute("departmentForm", departmentForm);
+        return "departmentAdd";
+    }
+
+    @PostMapping("/create")
+    String create(Model model, @ModelAttribute("brigadeForm") DepartmentForm departmentForm){
+        Department department = new Department();
+        department.setName(departmentForm.getName());
+        department.setAdress(departmentForm.getAdress());
+        department.setAdministrationID(departmentForm.getAdministrationID());
+        department.setNumberOfBrigates(departmentForm.getNumberOfBrigates());
+        department.setDescriction(departmentForm.getDescriction());
+        service.save(department);
+        model.addAttribute("departments", service.getall());
+        return "redirect:/web/department/get/list";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    String edit(Model model, @PathVariable("id") String id){
+        Department department = service.get(id);
+        DepartmentForm departmentForm = new DepartmentForm();
+        departmentForm.setName(department.getName());
+        departmentForm.setAdress(department.getAdress());
+        departmentForm.setAdministrationID(department.getAdministrationID());
+        departmentForm.setNumberOfBrigates(department.getNumberOfBrigates());
+        departmentForm.setDescriction(department.getDescriction());
+        model.addAttribute("departmentForm", departmentForm);
+        return "departmentAdd";
+    }
+
+    @PostMapping("/edit/{id}")
+    String edith(Model model, @PathVariable("id") String id, @ModelAttribute("departmentForm") DepartmentForm departmentForm){
+        Department department = new Department();
+        department.setId(id);
+        department.setName(departmentForm.getName());
+        department.setAdress(departmentForm.getAdress());
+        department.setAdministrationID(departmentForm.getAdministrationID());
+        department.setNumberOfBrigates(departmentForm.getNumberOfBrigates());
+        department.setDescriction(departmentForm.getDescriction());
+        service.save(department);
+        model.addAttribute("departments", service.getall());
+        return "redirect:/web/department/get/list";
     }
 }
--- src/main/java/edu/holeiden/coursework/controller/web/PassengerWEBController.java
+++ src/main/java/edu/holeiden/coursework/controller/web/PassengerWEBController.java
@@ -1,13 +1,14 @@
 package edu.holeiden.coursework.controller.web;
 
+import edu.holeiden.coursework.form.DepartmentForm;
+import edu.holeiden.coursework.form.PassengerForm;
+import edu.holeiden.coursework.model.Department;
 import edu.holeiden.coursework.model.Passenger;
 import edu.holeiden.coursework.service.passanger.impls.PassengerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,7 +29,48 @@ public class PassengerWEBController {
                   @PathVariable("id") String id){
         service.delete(id);
         model.addAttribute("passengers", service.getall());
-        return "passengerList";
+        return "redirect:/web/passenger/get/list";
+    }
+
+    @RequestMapping("/create")
+    String create(Model model){
+        PassengerForm passengerForm = new PassengerForm();
+        model.addAttribute("passengerForm", passengerForm);
+        return "passengerAdd";
+    }
+
+    @PostMapping("/create")
+    String create(Model model, @ModelAttribute("brigadeForm") PassengerForm passengerForm){
+        Passenger passenger = new Passenger();
+        passenger.setStatus(passengerForm.getStatus());
+        passenger.setRouteID(passengerForm.getRouteID());
+        passenger.setDescriction(passengerForm.getDescriction());
+        service.save(passenger);
+        model.addAttribute("passengers", service.getall());
+        return "redirect:/web/passenger/get/list";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    String edit(Model model, @PathVariable("id") String id){
+        Passenger passenger = service.get(id);
+        PassengerForm passengerForm = new PassengerForm();
+        passengerForm.setStatus(passenger.getStatus());
+        passengerForm.setRouteID(passenger.getRouteID());
+        passengerForm.setDescriction(passenger.getDescriction());
+        model.addAttribute("passengerForm", passengerForm);
+        return "passengerAdd";
+    }
+
+    @PostMapping("/edit/{id}")
+    String edith(Model model, @PathVariable("id") String id, @ModelAttribute("departmentForm") PassengerForm passengerForm){
+        Passenger passenger = new Passenger();
+        passenger.setId(id);
+        passenger.setStatus(passengerForm.getStatus());
+        passenger.setRouteID(passengerForm.getRouteID());
+        passenger.setDescriction(passengerForm.getDescriction());
+        service.save(passenger);
+        model.addAttribute("passengers", service.getall());
+        return "redirect:/web/passenger/get/list";
     }
 }
 
