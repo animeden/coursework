fd05b758dcce0064965e9a731c8daadaf574fc81
Denis Holei <goleiden@gmail.com>
2020-05-13 19:27:41 +0300

    first commit

--- src/main/java/edu/holeiden/coursework/controller/web/BrigadeWEBController.java
+++ src/main/java/edu/holeiden/coursework/controller/web/BrigadeWEBController.java
@@ -4,19 +4,26 @@ import edu.holeiden.coursework.form.AdministrationForm;
 import edu.holeiden.coursework.form.BrigadeForm;
 import edu.holeiden.coursework.model.Administration;
 import edu.holeiden.coursework.model.Brigade;
+import edu.holeiden.coursework.model.Department;
 import edu.holeiden.coursework.service.brigade.impls.BrigadeServiceImpl;
+import edu.holeiden.coursework.service.department.impls.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/web/brigade")
 public class BrigadeWEBController {
     @Autowired
     BrigadeServiceImpl service;
+
+    @Autowired
+    DepartmentServiceImpl departmentService;
 
     @RequestMapping("/get/list")
     String getall(Model model){
@@ -35,6 +42,9 @@ public class BrigadeWEBController {
     @RequestMapping("/create")
     String create(Model model){
         BrigadeForm brigadeForm = new BrigadeForm();
+        Map<String, String> mavs = departmentService.getall().stream()
+                .collect(Collectors.toMap(Department::getId, Department::getName));
+        model.addAttribute("mavs", mavs);
         model.addAttribute("brigadeForm", brigadeForm);
         return "brigadeAdd";
     }
@@ -42,9 +52,10 @@ public class BrigadeWEBController {
     @PostMapping("/create")
     String create(Model model, @ModelAttribute("brigadeForm") BrigadeForm brigadeForm){
         Brigade brigade = new Brigade();
+        Department department = departmentService.get(brigadeForm.getDepartmentId());
         brigade.setMission(brigadeForm.getMission());
         brigade.setNumberOfWorkers(brigadeForm.getNumberOfWorkers());
-        brigade.setDepartmentId(brigadeForm.getDepartmentId());
+        brigade.setDepartmentId(department);
         brigade.setDescriction(brigadeForm.getDescriction());
         service.save(brigade);
         model.addAttribute("brigades", service.getall());
@@ -55,10 +66,13 @@ public class BrigadeWEBController {
     String edit(Model model, @PathVariable("id") String id){
         Brigade brigade = service.get(id);
         BrigadeForm brigadeForm = new BrigadeForm();
+        Map<String, String> mavs = departmentService.getall().stream()
+                .collect(Collectors.toMap(Department::getId, Department::getName));
         brigadeForm.setMission(brigade.getMission());
         brigadeForm.setNumberOfWorkers(brigade.getNumberOfWorkers());
-        brigadeForm.setDepartmentId(brigade.getDepartmentId());
+        brigadeForm.setDepartmentId(brigade.getDepartmentId().getName());
         brigadeForm.setDescriction(brigade.getDescriction());
+        model.addAttribute("mavs", mavs);
         model.addAttribute("brigadeForm", brigadeForm);
         return "brigadeAdd";
     }
@@ -66,10 +80,11 @@ public class BrigadeWEBController {
     @PostMapping("/edit/{id}")
     String edith(Model model, @PathVariable("id") String id, @ModelAttribute("brigadeForm") BrigadeForm brigadeForm){
         Brigade brigade = new Brigade();
+        Department department = departmentService.get(brigadeForm.getDepartmentId());
         brigade.setId(id);
         brigade.setMission(brigadeForm.getMission());
         brigade.setNumberOfWorkers(brigadeForm.getNumberOfWorkers());
-        brigade.setDepartmentId(brigadeForm.getDepartmentId());
+        brigade.setDepartmentId(department);
         brigade.setDescriction(brigadeForm.getDescriction());
         service.save(brigade);
         model.addAttribute("brigades", service.getall());
